13adec746473965dbdfb15461cb25c2515b74429
dagregi <dagim6066@gmail.com>
2023-03-25 16:17:58 +0300

    Fixed some bugs

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,23 +9,20 @@ export const getData = (id: string) => axios.get<Goal>(`${BASE_URL}/${id}`).then
 
 export const removeGoal = (id: string) => axios.delete(`${BASE_URL}/${id}`)
 
-export const addGoal = (goal: Goal) => axios.post<Goal>(BASE_URL, {
-  headers: {
-    'Content-type': 'application/json',
-    Accept: 'application/json',
-  },
-  body: JSON.stringify(goal),
+export const addGoal = (goal: Goal) => axios({
+  method: 'post',
+  url: BASE_URL,
+  data: goal,
 }).then((response) => response.data)
 
 export const updateHour = async (id: string, hour: number): Promise<Goal> => {
   const oldGoal = await getData(id);
   const updatedGoal = { ...oldGoal, hours: hour };
-  const response = await axios.put(BASE_URL, {
-    headers: {
-      'Content-type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(updatedGoal),
+  const response = await axios({
+    method: 'put',
+    url: `${BASE_URL}/${id}`,
+    data: updatedGoal,
   })
   return response.data as Goal
 }
+
